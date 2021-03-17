f928f3c5717e76ae1782b5576a1af5bf8fd8b3af
Nauja <jeremymorosi@hotmail.fr>
2021-03-17 17:48:08 +0100

    init

---
+++ gadalang_imaging/__init__.py
@@ -0,0 +1,17 @@
+import sys
+
+
+def hello_world(argv=None):
+    print("hello world !")
+
+
+def echo():
+    pass
+
+
+def main():
+    print("hello world !")
+
+
+if __name__ == "__main__":
+    main()
